ce41517d69e0f6fa0bdf2c7cc6cc59f7498dc5f6
Clay <claycarney7@gmail.com>
2022-05-23 18:38:34 -0400

    remove previous alerts; add semi colons to js code; add simple calculator function to practice site using two prompts and one alert

--- clay.js
+++ clay.js
@@ -1,46 +1,42 @@
-console.log("Hello World")
-console.log("Hola Terra!")
+console.log("Hello World");
+console.log("Hola Terra!");
 const Max="57";
-let actual
-actual=Max-13
-let percentage
-percentage=actual/Max
+let actual;
+actual=Max-13;
+let percentage;
+percentage=actual/Max;
 console.log(percentage);
-let message
-message="hello"
-message=123456
-console.log(message)
-let n=123
-n=12.345
-console.log(n)
+let message;
+message="hello";
+message=123456;
+console.log(message);
+let n=123;
+n=12.345;
+console.log(n);
 const bigInt = 1234567890123456789012345678901234567890n;
-phrase=`can embed another ${1+2}!`
-let isGreater=4>1
-let age
-typeof x
-const string='The revolution will not be televised.'
-console.log(string)
+phrase=`can embed another ${1+2}!`;
+let isGreater=4>1;
+let age;
+typeof x;
+const string='The revolution will not be televised.';
+console.log(string);
 const badString=string;
-console.log(badString)
-const bigMouth='I\'ve got no right to take my place...'
-console.log(bigMouth)
+console.log(badString);
+const bigMouth='I\'ve got no right to take my place...';
+console.log(bigMouth);
 const name= 'Chris';
-const greeting= `Hello, ${"name"}`
-console.log(greeting)
+const greeting= `Hello, ${"name"}`;
+console.log(greeting);
 const one='Hello, ';
 const two='how are you?';
 const joined=`${one}${two}`;
-console.log(joined)
+console.log(joined);
 const three='Hola, ';
 const four='como estas?';
-const together=`${three}${four}${" Here is Johnny"}`
-console.log(together)
-alert(2>1)
-alert(2==1)
-alert(2!=1)
+const together=`${three}${four}${" Here is Johnny"}`;
+console.log(together);
 let result=5>4;
-alert(result)
-alert('Z'>'A')
-if (hour>18) {
-    greeting= "Good day";
-}
+var first=prompt("enter first number");
+var second=prompt("enter second number");
+var sum=Number(first) + Number(second);
+alert("The sum is: " + sum + "!");
